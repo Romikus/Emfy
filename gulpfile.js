645c94303a0df749ab4cea858c4b5b33344e9c70
Roman Naumov <romas.naumov@gmail.com>
2017-04-12 16:57:43 +0300

    Верстка проекта TEST

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,11 @@ var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
 var browserSync = require("browser-sync").create();
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
     var prefixer = [autoprefixer({browsers: ["last 2 version"]})];
@@ -14,7 +18,10 @@ gulp.task("style", function() {
     	.pipe(plumber())
         .pipe(sass())
         .pipe(postcss(prefixer))
-        .pipe(gulp.dest("css"))
+        .pipe(gulp.dest("build/css"))
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"))
         .pipe(browserSync.stream());
 });
 
@@ -29,4 +36,24 @@ gulp.task("serve", ["style"], function() {
 
     gulp.watch("sass/**/*{.scss,sass}", ["style"]);
     gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**",
+    "*.html"
+  ], {
+    base: "."
+  })
+  .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("build", function(fn) {
+  run("clean", "copy", "style", fn);
 });
